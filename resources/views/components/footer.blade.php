93cfe85a9103e47fa297bdff4b26bfddbd3aeb54
gotoroho <gotoroho@yandex.ru>
2021-06-12 22:40:50 +0300

    fix: rights text (not)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="footer__wrapper wrapper">
         {{--TODO: replace socials from header to footer and set in header site menu--}}
-        <p class="footer__copyright">Copyright © 1997 – {{ date("Y") }}. gotoroho. All rights reserved (not).</p>
+        <p class="footer__copyright">Copyright © 1997 – {{ date("Y") }}. gotoroho. All rights reserved (нет).</p>
 
         <p class="footer__made-by">Designed && developed by gotoroho</p>
     </div>
